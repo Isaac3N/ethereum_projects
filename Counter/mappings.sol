0399f5bd1bf3a88d41d48191e2856a38ab928519
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-06-03 14:20:31 +0100

    nested mappings in solidity

--- Counter/mappings.sol
+++ Counter/mappings.sol
@@ -6,10 +6,34 @@ contract MyContract {
     // mappings
 
     mapping(uint256 => string) public names;
+    //      key => value
+    mapping(uint256 => Book) public books;
+    mapping(address => mapping(uint256 => Book)) public myBooks;
 
-    constructor() {
+    struct Book {
+        string title;
+        string author;
+    }
+
+    constructor() public {
         names[1] = "adam";
         names[2] = "Isaac";
         names[3] = "Jumpman Adampage";
+    }
+
+    function addBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        books[_id] = Book(_title, _author);
+    }
+
+    function addMyBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
     }
 }
