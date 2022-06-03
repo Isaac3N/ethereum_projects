499f67b27503067ef2b0ea6bf66bc888ad340b8f
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-06-03 14:49:46 +0100

    conditionals in solidity

--- Counter/conditionals.sol
+++ Counter/conditionals.sol
@@ -3,6 +3,23 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract MyContract {
+    // Conditional
+    // Loop
+
+    uint256[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    function countEvenNumbers() public view returns (uint256) {
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < numbers.length; i++) {
+            if (isEvenNumber(numbers[i])) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     // Conditionals
     function isEvenNumber(uint256 _number) public pure returns (bool) {
         if (_number % 2 == 0) {
